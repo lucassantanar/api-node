13b01ea2bbca0b87f2985d7e725572f4350343e3
Lucas Santana <lucassice@gmail.com>
2021-02-28 18:15:48 -0300

    - Refatoração do SendMailController - Validação de usuário com biblioteca Yup - Função (alterar nota resposta) - Criação de controller para calculo de NPS - Personalização de erros com express-async-errors.

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveysController';
-import { UserController } from "./controllers/UserController"
+import { UserController } from "./controllers/UserController";
+import { AnswerController } from "./controllers/AnswerController";
+import { NpsController } from './controllers/NpsController';
 
 const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
+const npsController = new NpsController();
 
 router.post("/users", userController.create);
 
@@ -15,5 +19,9 @@ router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
 
 router.post("/sendMail", sendMailController.excute);
+
+router.get("/answers/:value", answerController.execute);
+
+router.get("/nps/:survey_id", npsController.execute);
 
 export { router };
